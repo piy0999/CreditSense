0e253b750559f0cb3778aa3193988645456f933d
Piyush Jha <piy0999@hku.hk>
2018-03-05 16:46:57 +0800

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,15 +1,13 @@
+pragma solidity ^0.4.0;
 contract SimpleStorage {
-  uint storedData;
+  string storedData;
 
-  function SimpleStorage() {
-    storedData = 1;
+  function SimpleStorage(string entry) {
+    storedData = entry;
   }
 
-  function set(uint x) {
-    storedData = x;
-  }
-
-  function get() constant returns (uint) {
+  function get() constant returns (string) {
     return storedData;
   }
 }
+
