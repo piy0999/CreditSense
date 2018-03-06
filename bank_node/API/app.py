d8366d0a22a8d84a238ca72a9e879da1eab1061a
Waqas Ali <waqas.abbasi@outlook.com>
2018-03-07 01:55:54 +0800

    multichain connect

--- bank_node/API/app.py
+++ bank_node/API/app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from Savoir import Savoir
+import json
+
+api = Savoir(rpcuser, rpcpasswd, rpchost, rpcport, chainname)
+api.getinfo()
 
 app = Flask(__name__)
+
+multichain = connect()
+
+def connect():
+    with open('credentials.json') as json_data:
+        credentials = json.loads(json_data)
+        json_data.close()
+    rpcuser = credentials.rpcuser
+    rpcpasswd = credentials.rpcpasswd
+    rpchost = credentials.rpchost
+    rpcport = credentials.rpcport
+    chainname = credentials.chainname
+    return Savoir(rpcuser, rpcpasswd, rpchost, rpcport, chainname)
 
 @app.route('/')
 def index():
@@ -10,13 +28,14 @@ def index():
 def add_loan_applicat_data():
     if request.method == 'POST':
         data = request.form
-
-    return 'Success'
+        return 'Success'
 
 @app.route('/get_all_applicant_data',methods=['GET'])
 def get_all_applicant_data():
     if request.method == 'GET':
         data =
     return 'choddiya tarun69 ko'
+
 if __name__ == "__main__":
+
     app.run(debug=True)
