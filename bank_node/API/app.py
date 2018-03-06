f6f6feb7633cc3601c05abc7a6b3c0e8d95db082
Waqas Ali <waqas.abbasi@outlook.com>
2018-03-07 01:43:06 +0800

    install multichain wrapper

--- bank_node/API/app.py
+++ bank_node/API/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return 'This is the homepage'
 
+@app.route('/add_loan_applicant_data',methods=['POST'])
+def add_loan_applicat_data():
+    if request.method == 'POST':
+        data = request.form
+
+    return 'Success'
+
+@app.route('/get_all_applicant_data',methods=['GET'])
+def get_all_applicant_data():
+    if request.method == 'GET':
+        data =
+    return 'choddiya tarun69 ko'
 if __name__ == "__main__":
     app.run(debug=True)
