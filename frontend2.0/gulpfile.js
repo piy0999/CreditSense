d78b71e03da24b03bf24ed7701ca545ba0b3a02a
Waqas Ali <waqas.abbasi@outlook.com>
2018-05-11 04:17:49 +0800

    frontendLast

--- frontend2.0/gulpfile.js
+++ frontend2.0/gulpfile.js
@@ -1,54 +1,52 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer');
-    browserSync = require('browser-sync').create();
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer');
+browserSync = require('browser-sync').create();
 
 var DEST = 'build/';
 
 gulp.task('scripts', function() {
-    return gulp.src([
-        'src/js/helpers/*.js',
-        'src/js/*.js',
-      ])
-      .pipe(concat('custom.js'))
-      .pipe(gulp.dest(DEST+'/js'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest(DEST+'/js'))
-      .pipe(browserSync.stream());
+  return gulp
+    .src(['src/js/helpers/*.js', 'src/js/*.js'])
+    .pipe(concat('custom.js'))
+    .pipe(gulp.dest(DEST + '/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest(DEST + '/js'))
+    .pipe(browserSync.stream());
 });
 
 // TODO: Maybe we can simplify how sass compile the minify and unminify version
-var compileSASS = function (filename, options) {
+var compileSASS = function(filename, options) {
   return sass('src/scss/*.scss', options)
-        .pipe(autoprefixer('last 2 versions', '> 5%'))
-        .pipe(concat(filename))
-        .pipe(gulp.dest(DEST+'/css'))
-        .pipe(browserSync.stream());
+    .pipe(autoprefixer('last 2 versions', '> 5%'))
+    .pipe(concat(filename))
+    .pipe(gulp.dest(DEST + '/css'))
+    .pipe(browserSync.stream());
 };
 
 gulp.task('sass', function() {
-    return compileSASS('custom.css', {});
+  return compileSASS('custom.css', {});
 });
 
 gulp.task('sass-minify', function() {
-    return compileSASS('custom.min.css', {style: 'compressed'});
+  return compileSASS('custom.min.css', { style: 'compressed' });
 });
 
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './',
-            serveStaticOptions: {
-              extensions: ['html']
-            }
-        },
-        notify: false,
-        startPath: './bank/loan_application'
-    });
+  browserSync.init({
+    server: {
+      baseDir: './',
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    },
+    notify: false,
+    startPath: './bank/dashboard'
+  });
 });
 
 gulp.task('watch', function() {
